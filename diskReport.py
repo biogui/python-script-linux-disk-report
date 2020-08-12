76dd2cae2502eeb0c74b37e9c0a16e5ad03bd8fa
biogui <gmrtri@hotmail.com>
2020-08-12 02:52:25 -0300

    Finalize first version data's print module

--- diskReport.py
+++ diskReport.py
@@ -1,25 +1,49 @@
 #!/usr/bin/python3
-from sys import argv
 from re import search
+from sys import argv
+from math import ceil
 import os
+
+TAB = ' ' * 4
+fullData = list()
+
+class Dirr:
+	def __init__(self, lv, name, size, nFiles, nFolders):
+		self.lv = lv
+		self.name = name
+		self.size = size
+		self.nFiles = nFiles
+		self.nFolders = nFolders
+
+	def __repr__(self):
+		idt = TAB * (self.lv - 1)
+		sizeInfo = "{}Size              >> {} bytes\n".format(idt, self.size)
+
+		e = ceil((len(sizeInfo) - len(idt) - len(self.name) - 1) / 2)
+		edge = '-' * e
+
+		title = "{}{}{}{}\n".format(idt, edge, self.name, edge)
+		filesInfo = "{}Amount of files   >> {}\n".format(idt, self.nFiles)
+		foldersInfo = "{}Amount of folders >> {}\n".format(idt, self.nFolders)
+
+		return "{}{}{}{}".format(title, sizeInfo, filesInfo, foldersInfo)
 
 def strFromChar(origStr, ref):
 	init = 0
 	for i, c in enumerate(origStr):
-		if c == ref: init = i
+		if c == ref: init = i+1
 
 	return origStr[init:]
 
-def report(initialPath, nv):
-	indentation = "\t" * nv
-
+def report(path, nv):
+	indentation = TAB * nv
 	ignore = r"/\."
 
 	folderSize = 0
 	nFolders = 0
 	nFiles = 0
 
-	for entry in os.scandir(initialPath):
+	for entry in os.scandir(path):
 		name = os.path.basename(entry)
 
 		if search(ignore, entry.path): continue
@@ -40,6 +64,11 @@ def report(initialPath, nv):
 			nFiles += 1
 			folderSize += size
 
+	folderName = strFromChar(path, '/')
+	folderData = Dirr(nv, folderName, folderSize, nFiles, nFolders)
+
+	fullData.append(folderData)
+
 	return folderSize, nFolders, nFiles
 
 def main():
@@ -51,12 +80,14 @@ def main():
 	directory = argv[1]
 	name = strFromChar(directory, '/')
 
-	edge = ' - ' * 10
-	print("{}TREE{}".format(edge, edge))
+	edge = ' - ' * 9
+	print("\n- {}TREE{} -".format(edge, edge))
 	print("↳/{}".format(name))
 
 	report(directory, 1)
 
+	print("\n\n- {}DATA{} -\n".format(edge, edge))
+	for folder in fullData[::-1]: print(folder)
 
 
 if __name__ == "__main__":
